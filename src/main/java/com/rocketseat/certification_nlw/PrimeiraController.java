2e56a84681944cf9814b27d4bd37e538f9e0f2a8
Alberto Lucas <alberto.trindade@ufba.br>
2024-02-11 15:55:28 -0300

    First Controller

--- src/main/java/com/rocketseat/certification_nlw/PrimeiraController.java
+++ src/main/java/com/rocketseat/certification_nlw/PrimeiraController.java
@@ -1,5 +1,28 @@
 package com.rocketseat.certification_nlw;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/primeiraController")
 public class PrimeiraController {
+    @GetMapping("/retornarPrimeiraController")
+    public Usuario retornoPrimeiraController(){
+        var usuario = new Usuario("Lucas", 25); 
+        return usuario;
+    }
+
+    @PostMapping("/meuPrimeiroPost")
+    public String primeiroPost() {
+        return "Meu primeiro post!";
+    }
     
+
+    record Usuario(String nome ,int idade){
+    }
+
 }
